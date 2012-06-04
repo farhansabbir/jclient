1f9708c2c6be00376a9ab18ab78df8df96db6f71
Farhan <fsabbir@gmail.com>
2012-06-04 19:46:34 +0600

    jclient commit

--- src/jclient/Downloader.java
+++ src/jclient/Downloader.java
@@ -17,18 +17,19 @@ public class Downloader implements Runnable{
     private Hashtable OBJECTS;
     private boolean DONE;
     private HttpURLConnection HTTP_CONN;
-    private Updater updater;
+    private Updater UPDATER;
     private Thread THREAD;
     
     public Downloader(String url, Updater updater)
     {
+        this.UPDATER = updater;
         this.THREAD = new Thread(this);
         this.THREAD.start();
-        
     }
     
     public void run()
     {
+        System.out.println(this.UPDATER.isChangeLoaded());
         
     }
     
