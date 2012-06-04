3d8c80b23779a049da3d2b6c9a99200f119fab94
Farhan <fsabbir@gmail.com>
2012-06-04 16:20:13 +0600

    jclient commit

---
+++ src/jclient/Downloader.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jclient;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author fsabbir
+ */
+public class Downloader implements Runnable{
+    
+    private Hashtable OBJECTS;
+    private boolean DONE;
+    private HttpURLConnection HTTP_CONN;
+    
+    public Downloader(URL url, Hashtable objs)
+    {
+        
+    }
+    
+    public void run()
+    {
+        
+    }
+    
+}
